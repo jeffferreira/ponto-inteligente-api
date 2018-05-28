6aef49b6fbcf47d521de55edd13042c9c66257b0
Jefferson Ferreira <jefferson.ferreira@db1.com.br>
2018-05-28 00:22:05 -0300

    adicionando ehcache ao projeto

--- src/main/java/com/jeffferreira/pontointeligente/api/PontointeligenteApplication.java
+++ src/main/java/com/jeffferreira/pontointeligente/api/PontointeligenteApplication.java
@@ -2,8 +2,10 @@ package com.jeffferreira.pontointeligente.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class PontointeligenteApplication {
 
 	public static void main(String[] args) {
